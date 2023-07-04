3d008e2940d7c6e2807b92029a5e44e161551b30
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-07-04 10:58:09 +0300

    Update std.http client implementation

--- std-http-client/src/main.zig
+++ std-http-client/src/main.zig
@@ -1,52 +1,42 @@
+// Zig version: 0.11.0
+
 const std = @import("std");
 const http = std.http;
 
-// URL for fetching a random message.
-const RANDOM_MESSAGE_URL = "https://whatthecommit.com/index.txt";
-
-/// Returns a random message.
-fn getRandomMessage(allocator: std.mem.Allocator) ![]u8 {
-    // Create an HTTP client.
-    var client = http.Client{ .allocator = allocator };
-    // Release all associated resources with the client.
-    defer client.deinit();
-    // Parse the URI.
-    const uri = try std.Uri.parse(RANDOM_MESSAGE_URL);
-    // Create the headers that will be sent to the server.
-    var headers = std.http.Headers{ .allocator = allocator };
-    defer headers.deinit();
-    // Accept anything.
-    try headers.append("accept", "*/*");
-    // Make the connection to the server.
-    var request = try client.request(.GET, uri, headers, .{});
-    defer request.deinit();
-    // Send the request and headers to the server.
-    try request.start();
-    // Wait for the server to send use a response
-    try request.wait();
-    // Return the body.
-    return try request.reader().readAllAlloc(allocator, 8192);
-}
-
-/// Executes the `git commit` command with the given message parameter.
-fn commit(allocator: std.mem.Allocator, message: []const u8) !void {
-    var process = std.ChildProcess.init(&[_][]const u8{
-        "git",
-        "commit",
-        "-m",
-        message,
-    }, allocator);
-    try process.spawn();
-}
-
-/// Entry-point of the application.
 pub fn main() !void {
     // Create an allocator.
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    // Get a random message.
-    var message = try getRandomMessage(allocator);
-    defer allocator.free(message);
-    // Create a git commit.
-    try commit(allocator, message);
+
+    // Create an HTTP client.
+    var client = http.Client{ .allocator = allocator };
+    // Release all associated resources with the client.
+    defer client.deinit();
+
+    // Parse the URI.
+    const uri = std.Uri.parse("http://127.0.0.1:8000/get") catch unreachable;
+
+    // Create the headers that will be sent to the server.
+    var headers = std.http.Headers{ .allocator = allocator };
+    defer headers.deinit();
+
+    // Accept anything.
+    try headers.append("accept", "*/*");
+
+    // Make the connection to the server.
+    var request = try client.request(.GET, uri, headers, .{});
+    defer request.deinit();
+
+    // Send the request and headers to the server.
+    try request.start();
+
+    // Wait for the server to send use a response
+    try request.wait();
+
+    // Read the entire response body, but only allow it to allocate 8KB of memory.
+    const body = request.reader().readAllAlloc(allocator, 8192) catch unreachable;
+    defer allocator.free(body);
+
+    // Print out the response.
+    std.log.info("{s}", .{body});
 }
