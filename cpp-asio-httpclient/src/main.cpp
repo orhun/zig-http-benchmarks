6330c50d07d5231e7927eeb45a5e471bad5fbdc8
Matheus Catarino França <matheus-catarino@hotmail.com>
2023-08-14 10:38:00 -0300

    cpp: client refactor - improvement performance

--- cpp-asio-httpclient/src/main.cpp
+++ cpp-asio-httpclient/src/main.cpp
@@ -1,51 +1,101 @@
 #include <asio.hpp>
+#include <chrono>
 #include <iostream>
+#include <memory>
+#include <thread>
+#include <vector>
 
 using asio::ip::tcp;
 
+class HttpClient {
+public:
+  HttpClient(asio::io_context &ioContext)
+      : ioContext_(ioContext), socket_(ioContext) {}
+
+  void connect(const std::string &host, const std::string &port) {
+    tcp::resolver resolver(ioContext_);
+    endpoints_ = resolver.resolve(host, port);
+    asio::connect(socket_, endpoints_);
+  }
+
+  void sendRequest(const std::string &request,
+                   std::function<void(const std::string &)> onResponse) {
+    onResponse_ = std::move(onResponse);
+    asio::async_write(socket_, asio::buffer(request),
+                      [this](std::error_code ec, std::size_t /*length*/) {
+                        if (!ec) {
+                          readResponse();
+                        } else {
+                          std::cerr << "Write error: " << ec.message()
+                                    << std::endl;
+                        }
+                      });
+  }
+
+  void readResponse() {
+    asio::async_read_until(socket_, responseBuffer_, "\r\n\r\n",
+                           [this](std::error_code ec, std::size_t /*length*/) {
+                             if (!ec) {
+                               processResponse();
+                             } else {
+                               std::cerr << "Read error: " << ec.message()
+                                         << std::endl;
+                             }
+                           });
+  }
+
+  void processResponse() {
+    std::istream responseStream(&responseBuffer_);
+
+    std::string header;
+    while (std::getline(responseStream, header) && header != "\r") {
+      std::cout << header << std::endl;
+    }
+
+    std::ostringstream contentStream;
+    if (responseBuffer_.size() > 0) {
+      contentStream << &responseBuffer_;
+      onResponse_(contentStream.str());
+    }
+
+    socket_.close();
+  }
+
+private:
+  asio::io_context &ioContext_;
+  tcp::socket socket_;
+  tcp::resolver::results_type endpoints_;
+  asio::streambuf responseBuffer_;
+  std::function<void(const std::string &)> onResponse_;
+};
+
 int main() {
   try {
-    asio::io_context io_context;
+    asio::io_context ioContext;
 
-    // Resolve the server address and port
-    tcp::resolver resolver(io_context);
-    tcp::resolver::results_type endpoints =
-        resolver.resolve("localhost", "8000");
+    const std::string host = "localhost";
+    const std::string port = "8000";
 
-    // Create a socket and connect to the server
-    tcp::socket socket(io_context);
-    asio::connect(socket, endpoints);
+    std::vector<std::unique_ptr<HttpClient>> clients;
+    std::vector<std::string> responses;
 
-    // Prepare the HTTP request
-    std::string request = "GET /get HTTP/1.1\r\n"
-                          "Host: localhost\r\n"
-                          "Connection: close\r\n"
-                          "\r\n";
+    for (int i = 0; i < 1000; ++i) {
+      clients.emplace_back(std::make_unique<HttpClient>(ioContext));
+      clients.back()->connect(host, port);
+      clients.back()->sendRequest(
+          "GET /get HTTP/1.1\r\n"
+          "Host: localhost\r\n"
+          "Connection: keep-alive\r\n"
+          "\r\n",
+          [&](const std::string &response) { responses.push_back(response); });
+    }
 
-    // Send the request to the server
-    asio::write(socket, asio::buffer(request));
+    ioContext.run();
 
-    // Read and print the response from the server
-    asio::streambuf response_buffer;
-    asio::read_until(socket, response_buffer,
-                     "\r\n\r\n"); // Read until headers end
-    std::istream response_stream(&response_buffer);
-
-    // Print the response headers
-    std::string header;
-    while (std::getline(response_stream, header) && header != "\r")
-      std::cout << header << std::endl;
-
-    // Read and print the response content
-    std::string content;
-    if (response_buffer.size() > 0)
-      content.assign(asio::buffers_begin(response_buffer.data()),
-                     asio::buffers_end(response_buffer.data()));
-
-    std::cout << "Response Content: " << content << std::endl;
-
-    // Close the socket
-    socket.close();
+    // Process responses as needed
+    for (const auto &response : responses) {
+      std::cout << "Response Content: " << response << std::endl;
+    }
   } catch (const std::exception &e) {
     std::cerr << "Error: " << e.what() << std::endl;
   }
